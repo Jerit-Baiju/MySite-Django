66144f2edd69c88bc0899de885cf97495844120d
Jerit Baiju <jeritalumkal@gmail.com>
2022-06-18 21:04:37 +0530

    changed http res to render

--- api/views.py
+++ api/views.py
@@ -1,22 +1,32 @@
 from django.http import HttpResponse
 from base.models import AdminLog
 from base.views import push
+from django.shortcuts import render
 # Create your views here.
 
 
 def latest_log(request):
-    context = AdminLog.objects.get(name='api_log').latest_log
-    return HttpResponse(str(context))
+    context = {
+        'title':'Latest Log',
+        'dark': True,
+        'content': AdminLog.objects.get(name='api_log').latest_log
+    }
+    return render(request, 'api/main.html',context)
 
 
 def log(request):
-    context = AdminLog.objects.get(name='api_log').log
-    return HttpResponse(str(context).replace('\n','<br><br>'))
+    context = {
+        'title': 'Jerit Baiju | Logs',
+        'dark': True,
+        'content': str(AdminLog.objects.get(name='api_log').log).split('\n'),
+        'type': 'list'
+    }
+    return render(request, 'api/main.html', context)
 
 
-def clr_admin_log(request):
-    log = AdminLog.objects.get(name='api_log')
-    log.log = ''
-    log.save()
-    push('API LOG CLEARED')
-    return HttpResponse('Cleared')
+# def clr_admin_log(request):
+#     log = AdminLog.objects.get(name='api_log')
+#     log.log = ''
+#     log.save()
+#     push('API LOG CLEARED')
+#     return HttpResponse('Cleared')
