ecb668fb2aeabf9e7a6a107f7de29b64d024573d
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-01 22:21:27 +0530

    log admin exception

--- base/views.py
+++ base/views.py
@@ -27,25 +27,26 @@ def push(text):
 
 
 def log(request, data):
-    date = datetime.now(pytz.timezone("Asia/Kolkata")).date()
-    time = datetime.now(pytz.timezone("Asia/Kolkata")).strftime("%H:%M")
-    agent = request.META['HTTP_USER_AGENT']
-    admin_log, _ = AdminLog.objects.get_or_create(name='api_log')
+    if request.user != 'jerit':
+        date = datetime.now(pytz.timezone("Asia/Kolkata")).date()
+        time = datetime.now(pytz.timezone("Asia/Kolkata")).strftime("%H:%M")
+        agent = request.META['HTTP_USER_AGENT']
+        admin_log, _ = AdminLog.objects.get_or_create(name='api_log')
 
-    if request.user.is_authenticated:
-        user = request.user
-        body = f"{date} | {time} | {user} | {data} | {agent}"
-        admin_log.latest_log = body
-        admin_log.log = f"{body}\n{admin_log.log}"
-        admin_log.save()
-        user_log = f"{date} | {time} | {data} | {agent}\n{user.log}"
-        user.log = user_log
-        user.save()
-    else:
-        body = f"{date} | {time} | {data} | {agent}"
-        admin_log.latest_log = body
-        admin_log.log = f"{body}\n{admin_log.log}"
-        admin_log.save()
+        if request.user.is_authenticated:
+            user = request.user
+            body = f"{date} | {time} | {user} | {data} | {agent}"
+            admin_log.latest_log = body
+            admin_log.log = f"{body}\n{admin_log.log}"
+            admin_log.save()
+            user_log = f"{date} | {time} | {data} | {agent}\n{user.log}"
+            user.log = user_log
+            user.save()
+        else:
+            body = f"{date} | {time} | {data} | {agent}"
+            admin_log.latest_log = body
+            admin_log.log = f"{body}\n{admin_log.log}"
+            admin_log.save()
 
 
 intro = '''Hi, I'm Jerit. I am passionate about Coding and building things. I am
