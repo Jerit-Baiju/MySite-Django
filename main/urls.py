732014d6f6a3e07f24676ca329ef8695c04c39d0
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-16 17:06:05 +0530

    changed favicon

--- main/urls.py
+++ main/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('projects/', include('projects.urls')),
     path(f'api/', include('api.urls')),
     path('favicon.ico', RedirectView.as_view(
-        url=staticfiles_storage.url('logo.png')))
+        url=staticfiles_storage.url('favicon.ico')))
 ]
