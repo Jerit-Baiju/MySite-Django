47d081e4186b3f0d51cd803d018eda6356cf4747
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-17 23:07:52 +0530

    Updated API Content, Permissions

--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 from base.models import AdminLog
 from base.views import push
-from base.views import log as add_log
 from django.shortcuts import render
 from django.contrib.auth import authenticate
 # Create your views here.
@@ -19,14 +18,12 @@ def latest_log(request):
             'dark': True,
             'content': AdminLog.objects.get(name='api_log').latest_log
         }
-        if username != 'jerit':
-            add_log(request, 'Accessed Latest Log')
         return render(request, 'api/main.html', context)
     else:
         context = {
             'title': 'Latest Log',
             'dark': True,
-            'content': 'Auth Failed'
+            'content': 'Access Denied'
         }
         return render(request, 'api/main.html', context)
 
@@ -34,7 +31,7 @@ def latest_log(request):
 def log(request):
     username = request.GET.get('user')
     password = request.GET.get('pass')
-    if username in ['jerit', 'appuaa']:
+    if username == "jerit":
         user = authenticate(request, username=username, password=password)
     else:
         user = None
@@ -45,14 +42,12 @@ def log(request):
             'content': str(AdminLog.objects.get(name='api_log').log).split('\n'),
             'type': 'list'
         }
-        if username != 'jerit':
-            add_log(request, 'Accessed Logs')
         return render(request, 'api/main.html', context)
     else:
         context = {
             'title': 'Latest Log',
             'dark': True,
-            'content': 'Auth Failed'
+            'content': 'Access Denied'
         }
         return render(request, 'api/main.html', context)
 
@@ -79,6 +74,6 @@ def clr_admin_log(request):
         context = {
             'title': 'Latest Log',
             'dark': True,
-            'content': 'Auth Failed'
+            'content': 'Access Denied'
         }
         return render(request, 'api/main.html', context)
