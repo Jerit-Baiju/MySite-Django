bd34681cfaca08242467a3ea1f4aedd3e9fa8525
Jerit Baiju <jeritalumkal@gmail.com>
2022-08-10 11:19:51 +0530

    deleted add data models

--- api/models.py
+++ api/models.py
@@ -1,20 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Data(models.Model):
-    store = models.ForeignKey('DataStore', on_delete=models.CASCADE)
-    key = models.CharField(max_length=200)
-    data = models.TextField(null=True, blank=True)
-
-    def __str__(self):
-        return self.key
-
-
-class DataStore(models.Model):
-    name = models.CharField(max_length=200)
-    value = models.ManyToManyField(Data, blank=True)
-
-    def __str__(self):
-        return self.name
