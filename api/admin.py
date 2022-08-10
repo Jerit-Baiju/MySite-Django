1ed60ac3a7e47ea25e93b7b43b6152e18e1ecf33
Jerit Baiju <jeritalumkal@gmail.com>
2022-08-10 11:25:04 +0530

    deleted store

--- api/admin.py
+++ api/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-from .models import DataStore, Data
 
 # Register your models here.
-
-class DataAdmin(admin.ModelAdmin):
-    list_display = ['store','key', 'data']
-    ordering = ['store','key']
-
-
-admin.site.register(DataStore)
-admin.site.register(Data, DataAdmin) 
