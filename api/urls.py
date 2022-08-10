d3306129d31c7be0bccbf4131b0cd3a113a5ae2f
Jerit Baiju <jeritalumkal@gmail.com>
2022-08-10 11:20:17 +0530

    deleted add store url

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('latest/', views.latest_log, name='latest_log'),
     path('log/', views.log, name='log'),
     path('clr/', views.clr_admin_log, name='clr_log'),
-    path('store/<str:store>',views.add_data)
 ]
