cc706be4691f7a40649ec5aaded53fa0dee28507
Jerit Baiju <jeritalumkal@gmail.com>
2022-11-10 20:36:29 +0530

    .

--- base/models.py
+++ base/models.py
@@ -1,18 +1,52 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 # Create your models here.
 
 
+class UserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
 class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
-    username = models.CharField(unique=True, max_length=200, null=True)
-    email = models.EmailField(max_length=200)
+    username = None
+    email = models.EmailField(max_length=200, unique=True)
     log = models.TextField(null=True, blank=True)
     score = models.IntegerField(null=True, blank=True)
-    USERNAME_FIELD = 'username'
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     def __str__(self):
-        return f"{self.name} - {self.username}"
+        return self.email
 
 
 class AdminLog(models.Model):
