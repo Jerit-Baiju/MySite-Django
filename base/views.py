9b57266e8a1c9c3907082a91704ef85147b74bf1
Jerit Baiju <jeritalumkal@gmail.com>
2022-11-22 15:54:22 +0530

    .,

--- base/views.py
+++ base/views.py
@@ -33,17 +33,16 @@ def log(request, data):
     time = datetime.now(pytz.timezone("Asia/Kolkata")).strftime("%I:%M %p")
     agent = request.META['HTTP_USER_AGENT']
     admin_log, _ = AdminLog.objects.get_or_create(name='api_log')
-    if request.user.is_authenticated and request.user.is_superuser:
-        None
-    elif request.user.is_authenticated:
-        user = request.user
-        body = f"{date} | {time} | {user} | {data} | {agent}"
-        admin_log.latest_log = body
-        admin_log.log = f"{body}\n{admin_log.log}"
-        admin_log.save()
-        user_log = f"{date} | {time} | {data} | {agent}\n{user.log}"
-        user.log = user_log
-        user.save()
+    if request.user.is_authenticated:
+        if not request.user.is_superuser:
+            user = request.user
+            body = f"{date} | {time} | {user} | {data} | {agent}"
+            admin_log.latest_log = body
+            admin_log.log = f"{body}\n{admin_log.log}"
+            admin_log.save()
+            user_log = f"{date} | {time} | {data} | {agent}\n{user.log}"
+            user.log = user_log
+            user.save()
     else:
         body = f"{date} | {time} | {data} | {agent}"
         admin_log.latest_log = body
