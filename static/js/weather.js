081c7eed9950bcb255b999a1d8c898b38e064e63
Jerit Baiju <jeritalumkal@gmail.com>
2022-12-04 16:04:17 +0530

    .

--- static/js/weather.js
+++ static/js/weather.js
@@ -1,15 +1,23 @@
-async function getData(){
-    alert(city)
-    url = 'https://192.168.43.21:5000/api/weather/' + city
-    await fetch(url).then((response) => response.json()).then((data) => alert(data))
-    alert(response)
+async function getData(city){
+    url = 'http://192.168.43.21:5000/api/weather/' + city
+  	const response = await fetch(url);
+  	var data = await response.json();
+  	var image = '<img id="image" src="'+ data["image_url"] +'" alt="weather_image" srcset="" />'
+  	
+  	$("#bar").show()
+  	$("#context").empty()
+  	var location = "<h4>"+ data["temp"] +"</h4>"
+  	$("#context").append(location)
+  	$("#image_holder").empty()
+		$("#image_holder").append(image)
+
+  	
 }
 $(document).ready(function () {
     $("#submit_btn").click(function () {
         city = document.getElementById("city_name").value
-        getData()
+        getData(city)
         
     })
-    // $("#bar").hide()
-    // $("#image").hide()
+    $("#bar").hide()
 })
