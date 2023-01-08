e0fb39ea277eaf963f1f3abdf8654b8dfacd49e3
Jerit Baiju <jeritalumkal@gmail.com>
2023-01-08 21:06:11 +0530

    env var

--- main/settings.py
+++ main/settings.py
@@ -92,25 +92,17 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-if os.environ['venv'] == 'dev':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-    
-else:
-    DATABASES = {
-        'default': {
-        	'ENGINE': "django.db.backends.postgresql_psycopg2",
-        	'HOST': "db.mtqdcoenyysdlxnkcriq.supabase.co",
-        	'NAME': "postgres",
-        	'USER': "postgres",
-        	'PASSWORD': os.environ['db'],
-        	'PORT': "5432",
-        	}
-    }
+
+DATABASES = {
+    'default': {
+      'ENGINE': "django.db.backends.postgresql_psycopg2",
+      'HOST': "db.mtqdcoenyysdlxnkcriq.supabase.co",
+      'NAME': "postgres",
+      'USER': "postgres",
+      'PASSWORD': os.environ['db'],
+      'PORT': "5432",
+      }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
