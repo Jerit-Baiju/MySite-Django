1a91269fee9fc2d2819e79a76719a68c2afadc37
Jerit Baiju <jeritalumkal@gmail.com>
2023-01-08 17:00:32 +0530

    .

--- main/settings.py
+++ main/settings.py
@@ -90,10 +90,10 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
