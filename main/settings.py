48bd2b6cd42cb669843ffe3848308efdf2c2ea32
Jerit Baiju <jeritalumkal@gmail.com>
2023-01-11 19:57:01 +0530

    set debug as env-var

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ['environ']
 
 AUTH_USER_MODEL = 'base.User'
 
