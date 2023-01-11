b7723a23ade889ad49775098be3be8e26700a49f
Jerit Baiju <jeritalumkal@gmail.com>
2023-01-11 19:28:59 +0530

    debug false

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 AUTH_USER_MODEL = 'base.User'
 
