f032d3fb347bb583302d5c069c1787de6edbeda0
Jerit Baiju <jeritalumkal@gmail.com>
2023-01-22 15:30:16 +0530

    added more cmds

--- guide.py
+++ guide.py
@@ -3,12 +3,18 @@ while True:
     main = input('--> ')
     if main == 'ssh' or main == 'server' or main == 'jerit.ml':
         os.system(
-            'ssh -i "server.pem" ubuntu@ec2-13-114-210-67.ap-northeast-1.compute.amazonaws.com')
+            'ssh -i "server.pem" ubuntu@ec2-3-111-55-45.ap-south-1.compute.amazonaws.com')
     elif main == "run":
         os.system('python manage.py runserver')
     elif main == 'x' or main == 'exit':
         exit()
     elif main == 'clear' or main == 'clr':
         os.system('clear')
+    elif main == 'push':
+        os.system('git push')
+    elif main == 'status':
+        os.system('git status')
+    elif main == 'pull':
+        os.system('git pull')
     else:
         print('>>> Unknown command')
