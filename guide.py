1dd80f43f55b187fda39c2e13cb8635cbf0a0cea
Jerit <jeritalumkal@gmail.com>
2023-02-01 20:22:39 +0530

    changed ip

--- guide.py
+++ guide.py
@@ -3,7 +3,7 @@ while True:
     main = input('--> ')
     if main == 'ssh' or main == 'server' or main == 'jerit.ml':
         os.system(
-            'ssh -i "server.pem" ubuntu@ec2-65-2-6-129.ap-south-1.compute.amazonaws.com')
+            'ssh -i "server.pem" ubuntu@ec2-13-232-66-6.ap-south-1.compute.amazonaws.com')
     elif main == "run":
         os.system('python manage.py runserver')
     elif main == 'x' or main == 'exit':
