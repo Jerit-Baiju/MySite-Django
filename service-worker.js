bf6e522fe8025a5c907563b1a622df689c8b24d9
Jerit Baiju <Jeritalumkal@gmail.com>
2023-02-23 12:34:41 +0000

    .

--- service-worker.js
+++ service-worker.js
@@ -62,13 +62,13 @@ self.addEventListener('fetch', (event) => {
   }
 });
 
-self.addEventListener('push', function(event){
+self.addEventListener('push', function (event) {
   const data = event.data.json()
   event.waitUntil(
     self.ServiceWorkerRegistration.showNotification(data.head, {
       body: body.data,
       icon: data.icon,
-      data: {url: data.url}
+      data: { url: data.url }
     })
   )
 })
@@ -93,4 +93,37 @@ const showNotAllowed = (message) => {
   const button = document.querySelector('form>button');
   button.innerHTML = `${message}`;
   button.setAttribute('disabled', 'true');
+};
+
+
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  const outputData = outputArray.map((output, index) => rawData.charCodeAt(index));
+
+  return outputData;
+}
+
+const subscribe = async (reg) => {
+  const subscription = await reg.pushManager.getSubscription();
+  if (subscription) {
+    sendSubData(subscription);
+    return;
+  }
+
+  const vapidMeta = document.querySelector('meta[name="vapid-key"]');
+  const key = vapidMeta.content;
+  const options = {
+    userVisibleOnly: true,
+    // if key exists, create applicationServerKey property
+    ...(key && { applicationServerKey: urlB64ToUint8Array(key) })
+  };
+
+  const sub = await reg.pushManager.subscribe(options);
+  sendSubData(sub)
 };
