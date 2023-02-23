c2053d0418c7a3ff47e5428dd27fec6c26cc7bb1
Jerit Baiju <Jeritalumkal@gmail.com>
2023-02-23 17:04:07 +0000

    formatted

--- base/models.py
+++ base/models.py
@@ -1,9 +1,11 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
-import os
 import datetime
+import os
+
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
 # Create your models here.
 
 
@@ -61,8 +63,7 @@ class Video(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.user} - {self.title}" 
-    
+        return f"{self.user} - {self.title}"
 
 
 class MediaFile(models.Model):
