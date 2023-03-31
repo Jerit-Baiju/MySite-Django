c4c64b14110ec704a536ad688e0e3bb05887504e
Jerit Baiju <jeritalumkal@gmail.com>
2023-03-31 12:16:27 +0530

    firebase

--- static/js/firebase.mjs
+++ static/js/firebase.mjs
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "./node_modules/firebase/app";
-import { getAnalytics } from "./node_modules/firebase/analytics";
-import { getMessaging, getToken, onMessage } from "./node_modules/firebase/messaging"
-import { onBackgroundMessage } from "./node_modules/firebase/messaging/sw"
+import { initializeApp } from "/node_modules/firebase/app";
+import { getAnalytics } from "/node_modules/firebase/analytics";
+import { getMessaging, getToken, onMessage } from "/node_modules/firebase/messaging"
+import { onBackgroundMessage } from "/node_modules/firebase/messaging/sw"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
