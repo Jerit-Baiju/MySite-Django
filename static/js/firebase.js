93c794571f78528f506accc6eb2493963421a3ed
Jerit Baiju <jeritalumkal@gmail.com>
2023-03-31 11:43:11 +0530

    firebase

--- static/js/firebase.js
+++ static/js/firebase.js
@@ -1,21 +1,37 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getMessaging, getToken } from "firebase/messaging"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDnllf521t5GPwIGE9xgp9xn709Qto4lq0",
-  authDomain: "jerit-ml.firebaseapp.com",
-  projectId: "jerit-ml",
-  storageBucket: "jerit-ml.appspot.com",
-  messagingSenderId: "31137066487",
-  appId: "1:31137066487:web:31a7ead9dd2a081dcf92f2",
-  measurementId: "G-W0EZ7CVXGR"
+    apiKey: "AIzaSyDnllf521t5GPwIGE9xgp9xn709Qto4lq0",
+    authDomain: "jerit-ml.firebaseapp.com",
+    projectId: "jerit-ml",
+    storageBucket: "jerit-ml.appspot.com",
+    messagingSenderId: "31137066487",
+    appId: "1:31137066487:web:31a7ead9dd2a081dcf92f2",
+    measurementId: "G-W0EZ7CVXGR"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const messaging = getMessaging(app)
+
+getToken(messaging, { vapidKey: 'BC2fdyMeF44rKN5jlNybS4Z-9EhurCkUNTqbWs80OlTwwDuDuNYiXMbOv4t2-NK2ZXl57a-z17UqtcqCskskYbo' }).then((currentToken) => {
+    if (currentToken) {
+        // Send the token to your server and update the UI if necessary
+        // ...
+    } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+    }
+}).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+});
