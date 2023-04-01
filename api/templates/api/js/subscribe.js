f8620a893723507509f90a96f7e945eb007dc808
Jerit Baiju <Jeritalumkal@gmail.com>
2023-04-01 07:43:16 +0000

    removed logging

--- api/templates/api/js/subscribe.js
+++ api/templates/api/js/subscribe.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-analytics.js";
-import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-messaging.js";
+import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-messaging.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDnllf521t5GPwIGE9xgp9xn709Qto4lq0",
@@ -12,23 +12,17 @@ const firebaseConfig = {
     measurementId: "G-W0EZ7CVXGR"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const messaging = getMessaging(app)
 
 $(document).ready(function () {
     $("#subscribe").click(function () {
         console.log('Requesting permission...');
         Notification.requestPermission().then((permission) => {
             if (permission === 'granted') {
-                console.log('Notification permission granted.');
-                // Get registration token. Initially this makes a network call, once retrieved
-                // subsequent calls to getToken will return from cache.
                 const messaging = getMessaging();
                 getToken(messaging, { vapidKey: 'BC2fdyMeF44rKN5jlNybS4Z-9EhurCkUNTqbWs80OlTwwDuDuNYiXMbOv4t2-NK2ZXl57a-z17UqtcqCskskYbo' }).then((currentToken) => {
                     if (currentToken) {
-                        console.log(currentToken)
                         $.ajax({
                             type: "POST",
                             url: "{% url 'subscribe' %}",
@@ -41,13 +35,10 @@ $(document).ready(function () {
                         });
 
                     } else {
-                        // Show permission request UI
                         console.log('No registration token available. Request permission to generate one.');
-                        // ...
                     }
                 }).catch((err) => {
                     console.log('An error occurred while retrieving token. ', err);
-                    // ...
                 });
             }
         })
