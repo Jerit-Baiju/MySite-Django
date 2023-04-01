b8f1887d61345225956a550c9b6ec3f14d233acc
Jerit Baiju <Jeritalumkal@gmail.com>
2023-04-01 08:00:02 +0000

    formatted

--- templates/js/firebase-sw.js
+++ templates/js/firebase-sw.js
@@ -3,13 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js')
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDnllf521t5GPwIGE9xgp9xn709Qto4lq0",
-    authDomain: "jerit-ml.firebaseapp.com",
-    projectId: "jerit-ml",
-    storageBucket: "jerit-ml.appspot.com",
-    messagingSenderId: "31137066487",
-    appId: "1:31137066487:web:31a7ead9dd2a081dcf92f2",
-    measurementId: "G-W0EZ7CVXGR"
+  apiKey: "AIzaSyDnllf521t5GPwIGE9xgp9xn709Qto4lq0",
+  authDomain: "jerit-ml.firebaseapp.com",
+  projectId: "jerit-ml",
+  storageBucket: "jerit-ml.appspot.com",
+  messagingSenderId: "31137066487",
+  appId: "1:31137066487:web:31a7ead9dd2a081dcf92f2",
+  measurementId: "G-W0EZ7CVXGR"
 });
 
 const messaging = firebase.messaging();
