3afd62655d5aa547c03d0cf100622582d3d79f24
Jerit Baiju <Jeritalumkal@gmail.com>
2023-05-20 17:01:38 +0000

    alter Firebase

--- templates/js/service-worker.js
+++ templates/js/service-worker.js
@@ -1,4 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+/* global importScripts, firebase */
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js')
 
 const CACHE = "jerit-baiju";
 
@@ -64,4 +67,30 @@ self.addEventListener('fetch', (event) => {
       }
     })());
   }
+});
+
+// firebase
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDnllf521t5GPwIGE9xgp9xn709Qto4lq0",
+  authDomain: "jerit-ml.firebaseapp.com",
+  projectId: "jerit-ml",
+  storageBucket: "jerit-ml.appspot.com",
+  messagingSenderId: "31137066487",
+  appId: "1:31137066487:web:31a7ead9dd2a081dcf92f2",
+  measurementId: "G-W0EZ7CVXGR"
+});
+
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    image: payload.notification.image
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
 });
