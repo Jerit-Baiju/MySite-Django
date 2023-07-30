b0c53add8baece7b11c46d32e5aa7c9658ca42f3
Jerit Baiju <jeritalumkal@gmail.com>
2023-07-30 22:43:07 +0530

    updated firebase

--- templates/js/service-worker.js
+++ templates/js/service-worker.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
-importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js')
+importScripts('https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/9.18.0/firebase-messaging.js')
 
 const CACHE = "jerit-baiju";
 
