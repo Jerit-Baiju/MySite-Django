91b58cb73872d19862850cbf610fa23af9c2cf3a
Jerit Baiju <jeritalumkal@gmail.com>
2023-07-30 16:51:16 +0530

    fb update

--- templates/js/service-worker.js
+++ templates/js/service-worker.js
@@ -70,17 +70,6 @@ self.addEventListener('fetch', (event) => {
   }
 });
 
-// firebase
-
-firebase.initializeApp({
-  apiKey: "AIzaSyDnllf521t5GPwIGE9xgp9xn709Qto4lq0",
-  authDomain: "jerit-ml.firebaseapp.com",
-  projectId: "jerit-ml",
-  storageBucket: "jerit-ml.appspot.com",
-  messagingSenderId: "31137066487",
-  appId: "1:31137066487:web:31a7ead9dd2a081dcf92f2",
-  measurementId: "G-W0EZ7CVXGR"
-});
 
 const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
