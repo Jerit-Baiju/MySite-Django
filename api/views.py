1832473e6a8d83a4b03364b5ac4456ceff154e4d
Jerit Baiju <jeritalumkal@gmail.com>
2023-08-05 19:51:00 +0530

    removed quotes

--- api/views.py
+++ api/views.py
@@ -17,24 +17,6 @@ from base.models import AdminLog, AdminSecret
 load_dotenv()
 # Create your views here.
 
-quotes = [
-    '"Happiness is not something ready-made. It comes from your own actions." - Dalai Lama',
-    '"The purpose of our lives is to be happy." - Dalai Lama',
-    '"Happiness is a direction, not a place." - Sydney J. Harris',
-    '"The best way to cheer yourself is to try to cheer someone else up." - Mark Twain',
-    '"Happiness is when what you think, what you say, and what you do are in harmony." - Mahatma Gandhi',
-    '"The only thing that will make you happy is being happy with who you are." - Goldie Hawn',
-    '"Happiness is not a goal; it\'s a by-product." - Eleanor Roosevelt',
-    '"The secret of happiness is not in doing what one likes, but in liking what one does." - James M. Barrie',
-    '"Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful." - Albert Schweitzer',
-    '"Happiness is letting go of what you think your life is supposed to look like and celebrating it for everything that it is." - Mandy Hale',
-    '"Be happy with what you have. Be excited about what you want." - Alan Cohen',
-    '"The most important thing is to enjoy your life - to be happy - its all that matters." - Audrey Hepburn',
-    '"The only joy in the world is to begin." - Cesare Pavese',
-    '"Happiness is not in the mere possession of money; it lies in the joy of achievement, in the thrill of creative effort." - Franklin D. Roosevelt',
-    '"Be happy for this moment. This moment is your life." - Omar Khayyam'
-]
-
 
 @csrf_exempt
 def subscribe(request):
