474684d4fab74ae0b5c85514bd0d2c610a6521fb
Jerit Baiju <jeritalumkal@gmail.com>
2023-11-21 17:37:55 +0530

    updated bugs on sending notifications

--- templates/js/firebase-sw.js
+++ templates/js/firebase-sw.js
@@ -1,5 +1,7 @@
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
+);
 
 firebase.initializeApp({
   apiKey: "AIzaSyAF0ZxYZN1XEztSNPqpV77kAUGObLNc6SQ",
@@ -7,17 +9,14 @@ firebase.initializeApp({
   projectId: "jerit-in",
   storageBucket: "jerit-in.appspot.com",
   messagingSenderId: "791070201278",
-  appId: "1:791070201278:web:b3a13ecadda6a3533a55ef"
+  appId: "1:791070201278:web:b3a13ecadda6a3533a55ef",
 });
 
 const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    image: payload.notification.image,
-    icon: payload.data.icon
-  };
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
+  const {
+    data: { body, image, icon, title },
+  } = payload;
+  const notificationOptions = { body, image, icon };
+  self.registration.showNotification(title, notificationOptions);
 });
