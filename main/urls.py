c4ba9fea188afcae719fd25e6cbd2efb0d4baa24
Jerit Baiju <jeritalumkal@gmail.com>
2023-11-24 11:11:17 +0530

    formatted

--- main/urls.py
+++ main/urls.py
@@ -29,7 +29,6 @@ handler404 = 'base.views.custom_404'
 urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,
         {'document_root': settings.MEDIA_ROOT}),
-
     url(r'^static/(?P<path>.*)$', serve,
         {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
