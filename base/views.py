17f2c295c193095ffedb02fe0a51e7070bbe06ea
Jerit Baiju <jeritalumkal@gmail.com>
2024-01-23 22:44:19 +0530

    update: skills, history

--- base/views.py
+++ base/views.py
@@ -133,8 +133,8 @@ def home(request):
         'quote': random.choice(quotes),
         'intro': INTRO,
         'skills': [
-            'HTML/CSS/JS', 'Bootstrap', 'jQuery', 'Git', 'Python', 'Flask', 'Selenium', 'Django', 'Heroku',
-            'SEO', 'GSC', 'AWS', 'PyLint', 'Django REST framework', 'React JS', 'Next JS'
+            'HTML/CSS/JS', 'Bootstrap/Tailwind', 'jQuery', 'Git', 'Python', 'Flask', 'Selenium', 'Django', 'Heroku',
+            'SEO', 'GSC', 'AWS', 'DRF', 'React JS', 'Next JS', 'TypeScript',
         ],
         'education': [
             {
@@ -167,14 +167,14 @@ def about(request):
     log(request, 'About')
     history = [
         "In 2016, when I was in 4th grade and 10 years old, my parents bought me a laptop. I was ecstatic about it! I quickly learned how to control the cursor and type with ease using MS-Paint and Notepad.",
-        "In 2017, I began playing Microsoft's mini-games like Chess, Minesweeper, and others, and learned MS-Logo. I created numerous objects and enjoyed experimenting with them.",
-        "In 2018, at the age of 12, I joined an activity on Photoshop and Animation using Adobe Flash-Macromedia. As I explored the computer in my school, I stumbled upon an HTML file and became intrigued. Curious about its purpose, I approached my teacher, Reshmi Miss, and asked about it. She kindly taught me some basics of HTML, sparking my interest in programming.",
-        "In 2019, I built my own website using HTML exclusively and subsequently discovered other programming languages such as C++, Python, and JavaScript.",
-        "In 2020, I created my own chatbot using Visual Basic and independently upgraded my computer from Windows 7 to Windows 10. I began learning Python and developed simple command-line applications, as well as GUI applications for Windows using Tkinter.",
-        "In 2021, I transitioned to Linux and expanded my skills by learning Flask, CSS, and JavaScript. I published my website on PythonAnywhere, which unfortunately is currently unavailable. Additionally, I delved deeper into GIT, REACT, and some Data Science topics. I completed numerous projects including a ChatBot named Clara (web-based) and a Weather app (command-line interface), among others.",
-        "In 2022, I acquired proficiency in Django and deployed my website on HEROKU. Additionally, I developed a package called PYFLIT for FLASK users and created projects such as a Weather App (web-based) and a Number Game, among others.",
-        "In 2023, I upgraded my setup with a MacBook Air M1, funded by my first freelancing project. This marked my entry into the Apple ecosystem, replacing an old Ubuntu laptop. I optimized my website by deploying it on AWS, and expanded my skills with React.js, Next.js, Tailwind, and TypeScript, adding versatility to my web development expertise.",
-        "In 2024, at 18 years old, I'm navigating towards new horizons in my journey. Aiming to secure a job, I'm strategically honing my skills and exploring opportunities. Embracing growth, I continue to leverage my expertise while maintaining a forward-looking approach. The focus this year revolves around professional development, aligning with my goal of advancing in the ever-evolving tech landscape."
+        "At 11, I began playing Microsoft's mini-games like Chess, Minesweeper, and others, and learned MS-Logo. I created numerous objects and enjoyed experimenting with them.",
+        "At 12, I joined an activity on Photoshop and Animation using Adobe Flash-Macromedia. As I explored the computer in my school, I stumbled upon an HTML file and became intrigued. Curious about its purpose, I approached my teacher, Reshmi Miss, and asked about it. She kindly taught me some basics of HTML, sparking my interest in programming.",
+        "At 13, I built my own website using HTML exclusively and subsequently discovered other programming languages such as C++, Python, and JavaScript.",
+        "At 14, I created my own chatbot using Visual Basic and independently upgraded my computer from Windows 7 to Windows 10. I began learning Python and developed simple command-line applications, as well as GUI applications for Windows using Tkinter.",
+        "At 15, I transitioned to Linux and expanded my skills by learning CSS, JavaScript, GIT, and Flask. I published my website on PythonAnywhere, which unfortunately is currently unavailable. I completed numerous projects including a ChatBot named Clara and a Weather app, among others.",
+        "At 16, I acquired proficiency in Django and deployed my website on HEROKU. Additionally, I developed a package called PYFLIT for FLASK users and learned some other languages like Go, R, CPP and Java. ",
+        "At 17, I upgraded my setup with a MacBook Air M1, funded by my first freelancing project. This marked my entry into the Apple ecosystem, replacing an old Ubuntu laptop. I optimized my website by deploying it on AWS, and expanded my skills with React.js, Next.js, Tailwind, and TypeScript, adding versatility to my web development expertise.",
+        "At 18 (present), I'm navigating towards new horizons in my journey. Aiming to secure a job, I'm strategically honing my skills and exploring opportunities. Embracing growth, I continue to leverage my expertise while maintaining a forward-looking approach. The focus this year revolves around professional development, aligning with my goal of advancing in the ever-evolving tech landscape."
     ]
     context = {
         'title': 'About Me | Jerit Baiju',
